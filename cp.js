107d85e3b94a42186170120f45e64fc89414b2e0
Dzheremilz <trempejeremie@gmail.com>
2021-03-24 18:07:09 +0100

    fix path

--- cp.js
+++ cp.js
@@ -11,13 +11,12 @@ if (process.argv.length === 3) {
 }
 
 if (process.argv.length === 4) {
-
   if (fs.existsSync(process.argv[3]) && fs.lstatSync(process.argv[3]).isDirectory()) {
     if (process.argv[3] === '.') {
       console.log(`node cp.js: '${process.argv[2]}' and '${process.argv[3] + '/' + process.argv[2]}' are the same file`)
       process.exit(1)
     } else {
-      fs.copyFileSync(process.argv[2], process.argv[3] + '/' + process.argv[2])
+      fs.copyFileSync(process.argv[2], process.argv[3] + '/' + process.argv[2].split('/').reverse()[0])
       process.exit(0)
       //console.log(`\'${process.argv[2]}\' -> \'${process.argv[3] + '/' + process.argv[2]}\'`)
     }
@@ -32,8 +31,9 @@ if (process.argv.length > 4 && fs.existsSync(process.argv[process.argv.length - 
     if (process.argv[process.argv.length - 1] === '.') {
       console.log(`node cp.js: '${process.argv[i]}' and '${process.argv[process.argv.length - 1] + '/' + process.argv[i]}' are the same file`)
     } else {
-      fs.copyFileSync(process.argv[i], process.argv[process.argv.length - 1] + '/' + process.argv[i])
-      //console.log(`\'${process.argv[2]}\' -> \'${process.argv[3] + '/' + process.argv[2]}\'`)
+
+      fs.copyFileSync(process.argv[i], process.argv[process.argv.length - 1] + '/' + process.argv[i].split('/').reverse()[0])
+      //console.log(`\'${process.argv[i]}\' -> \'${process.argv[process.argv.length - 1] + '/' + process.argv[i]}\'`)
     }
   }
 } else {
